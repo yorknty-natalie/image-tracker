6deacfaff49352bb5ecc781f0c99550e832dc082
Joseph Lin <joseph@example.com>
2026-02-03 06:15:31 +0000

    Add Traditional Guard Bear root node

--- tracker_data.js
+++ tracker_data.js
@@ -1,1 +1,9 @@
-const trackerData = [];
+const trackerData = [
+    {
+        id: "root-bear",
+        title: "Traditional Guard Bear",
+        date: "2026-02-03",
+        fileId: "1OZgstTsGBsWysGquhb1pECWPbplwdFBV",
+        children: []
+    }
+];
