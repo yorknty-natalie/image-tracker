88a27855e1b2b251976c7510522fb5a4e13a81b8
Joseph Lin <joseph@example.com>
2026-02-03 02:41:20 +0000

    Initial commit for Image Tracker project

---
+++ tracker_data.js
@@ -0,0 +1,38 @@
+const trackerData = {
+    id: "root",
+    title: "Valentino Inspiration",
+    date: "2026-01-30",
+    fileId: "1qu4C0XYOxCpMU5p1_uXfx_Rxeq1G_acd",
+    children: [
+        {
+            id: "x-trends-v2",
+            title: "X-Trends Initial Set",
+            date: "2026-01-30",
+            fileId: "1rvLY7BMts2KPtilwqR6a7be9s9pjw4z_",
+            children: [
+                {
+                    id: "molded-shapes",
+                    title: "Molded Glass Shapes",
+                    date: "2026-01-30",
+                    fileId: "1ey-T1AeUXPqbJ4IoTre9KRd7Ha5mOSYz",
+                    children: [
+                        {
+                            id: "dynamic-trends-0202",
+                            title: "Daily Trends (Feb 02)",
+                            date: "2026-02-02",
+                            fileId: "1Un4aDptynlKyIhxyGnmg-gKrlMpsMZQI",
+                            children: []
+                        },
+                        {
+                            id: "daily-trends-0203",
+                            title: "Daily Trends (Feb 03)",
+                            date: "2026-02-03",
+                            fileId: "16UItKPWMlH4_-AELsAN9Slg12VgYR4ps",
+                            children: []
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
