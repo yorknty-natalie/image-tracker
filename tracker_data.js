41530e7d74e31f7aa73019f7eef654fcb7753fed
Joseph Lin <joseph@example.com>
2026-02-03 06:18:01 +0000

    Add Traditional Christmas Tree root node

--- tracker_data.js
+++ tracker_data.js
@@ -1,5 +1,12 @@
 const trackerData = [
     {
+        id: "root-tree",
+        title: "Traditional Christmas Tree",
+        date: "2026-02-03",
+        fileId: "1K7kCISekKzBrewSQAyqq7dr7tmpmmaAx",
+        children: []
+    },
+    {
         id: "root-bear",
         title: "Traditional Guard Bear",
         date: "2026-02-03",
